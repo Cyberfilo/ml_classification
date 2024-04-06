6c0c95f982dae55b48dfb2bb65db6cf29e4b9418
menghi filippo <filo.gametech@gmail.com>
2024-04-06 21:26:31 +0200

    Initial Commit

---
+++ classificazione campioniUITests/classificazione_campioniUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  classificazione_campioniUITestsLaunchTests.swift
+//  classificazione campioniUITests
+//
+//  Created by Filippo Mattia Menghi on 06/04/24.
+//
+
+import XCTest
+
+final class classificazione_campioniUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
